e5ac8956a948a5fe8e54f484ea3cb8233d46d2f3
Jonathan Hess <hessjc@google.com>
2022-10-24 21:36:13 -0600

    feat: Add generate to the makefile to put the code into a consistent state.

--- operator.mk
+++ operator.mk
@@ -20,9 +20,69 @@ SHELL = /usr/bin/env bash -o pipefail
 help: ## Display this help.
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
+.PHONY: generate
+generate:  ctrl_generate ctrl_manifests fmt vet reset_image add_copyright_header go_fmt yaml_fmt ## Runs code generation, format, and validation tools
+
+##@ Development targets
+
+.PHONY: ctrl_generate
+ctrl_generate: controller-gen ## use controller-gen to generate code containing DeepCopy, DeepCopyInto, and DeepCopyObject method implementations.
+	$(CONTROLLER_GEN) object:headerFile="hack/boilerplate.go.txt" paths="./..."
+
+.PHONY: fmt
+fmt: ## Run go fmt against code.
+	go fmt ./...
+
+.PHONY: vet
+vet: ## Run go vet against code.
+	go vet ./...
+
+.PHONY: go_fmt
+go_fmt: ## Automatically formats go files
+	go mod tidy
+	go run golang.org/x/tools/cmd/goimports@latest -w .
+
+yaml_fmt: ## Automatically formats all yaml files
+	go run github.com/UltiRequiem/yamlfmt@latest -w $(shell find . -iname '*.yaml' -or -iname '*.yml')
+
+.PHONY: add_copyright_header ## Adds the copyright header to any go or yaml file that is missing the header
+add_copyright_header: ## Add the copyright header
+	go run github.com/google/addlicense@latest *
+
+##@ Kubernetes configuration build steps
+
+.PHONY: ctrl_manifests
+ctrl_manifests: controller-gen ## Use controller-gen to generate WebhookConfiguration, ClusterRole and CustomResourceDefinition objects.
+	$(CONTROLLER_GEN) rbac:roleName=manager-role crd webhook paths="./..." output:crd:artifacts:config=config/crd/bases
+
+.PHONY: reset_image
+reset_image: kustomize
+	cd config/manager && $(KUSTOMIZE) edit set image controller=cloudsql-proxy-operator:latest
+
+
+
 ##@ Build Dependencies
 
 ## Location to install dependencies to
 LOCALBIN ?= $(shell pwd)/bin
 $(LOCALBIN):
 	test -d $@ || mkdir -p $@
+
+## Tool Binaries
+KUSTOMIZE ?= $(LOCALBIN)/kustomize
+CONTROLLER_GEN ?= $(LOCALBIN)/controller-gen
+
+## Tool Versions
+CONTROLLER_TOOLS_VERSION ?= v0.9.2
+KUSTOMIZE_VERSION ?= v4.5.2
+
+.PHONY: controller-gen
+controller-gen: $(CONTROLLER_GEN) ## Download controller-gen locally if necessary.
+$(CONTROLLER_GEN): $(LOCALBIN)
+	test -s $(LOCALBIN)/controller-gen || GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-tools/cmd/controller-gen@$(CONTROLLER_TOOLS_VERSION)
+
+KUSTOMIZE_INSTALL_SCRIPT ?= "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
+.PHONY: kustomize
+kustomize: $(KUSTOMIZE) ## Download kustomize locally if necessary.
+$(KUSTOMIZE): $(LOCALBIN)
+	test -s $(LOCALBIN)/kustomize || { curl -s $(KUSTOMIZE_INSTALL_SCRIPT) | bash -s -- $(subst v,,$(KUSTOMIZE_VERSION)) $(LOCALBIN); }
