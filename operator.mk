47b100b79840aff87f00d045dcbb4ad170395c35
Jonathan Hess <hessjc@google.com>
2022-10-31 09:06:01 -0600

    doc: improve documentation in operator.mk for deploy targets

--- operator.mk
+++ operator.mk
@@ -67,7 +67,7 @@ build: generate build_push_docker ## Builds and pushes the docker image to tag d
 test: generate go_test ## Run tests (but not internal/teste2e)
 
 .PHONY: deploy
-deploy:  build_push_docker deploy_with_kubeconfig ## Deploys the operator to the kubernetes cluster using envvar KUBECONFIG. Set $IMG envvar to the image tag.
+deploy:  build deploy_with_kubeconfig ## Deploys the operator to the kubernetes cluster using envvar KUBECONFIG. Set $IMG envvar to the image tag.
 
 ##
 # Development targets
@@ -117,27 +117,27 @@ ctrl_manifests: controller-gen # Use controller-gen to generate WebhookConfigura
 	$(CONTROLLER_GEN) rbac:roleName=manager-role crd webhook paths="./..." output:crd:artifacts:config=config/crd/bases
 
 .PHONY: reset_image
-reset_image: kustomize # Resets the image used in the kubernetes config to a default image.
+reset_image: kustomize # Reset the image used in the kubernetes config to a default image.
 	cd config/manager && $(KUSTOMIZE) edit set image controller=cloudsql-proxy-operator:latest
 
 .PHONY: update_image
-update_image: kustomize
+update_image: kustomize # Update the image used in the kubernetes config to $(IMG)
 	cd config/manager && $(KUSTOMIZE) edit set image controller=$(IMG)
 
-.PHONY: deploy_with_kubeconfig
+.PHONY: deploy_with_kubeconfig # Deploy the kubernetes configuration to the current default cluster using kubectl
 deploy_with_kubeconfig: install_certmanager install_crd deploy_operator
 
 .PHONY: install_certmanager
-install_certmanager:  ## Install the certmanager operator
+install_certmanager: kubectl # Install the cert-manager operator to manage the certificates for the operator webhooks
 	$(KUBECTL) apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
 	$(KUBECTL) rollout status deployment -n cloud-sql-proxy-operator-system cloud-sql-proxy-operator-controller-manager --timeout=90s
 
 .PHONY: install
-install_crd: ctrl_manifests kustomize ## Install CRDs into the K8s cluster specified in ~/.kube/config.
+install_crd: kustomize kubectl # Install CRDs into the K8s cluster using the kubectl default behavior
 	$(KUSTOMIZE) build config/crd | $(KUBECTL) apply -f -
 
 .PHONY: deploy_operator
-deploy_operator: ctrl_manifests kustomize ## Deploy controller to the K8s cluster specified in ~/.kube/config.
+deploy_operator: kustomize kubectl # Deploy controller to the K8s cluster using the kubectl default behavior
 	cd config/manager && $(KUSTOMIZE) edit set image controller=${IMG}
 	$(KUSTOMIZE) build config/default | $(KUBECTL) apply -f -
 	$(E2E_KUBECTL) rollout status deployment -n cloud-sql-proxy-operator-system cloud-sql-proxy-operator-controller-manager --timeout=90s
