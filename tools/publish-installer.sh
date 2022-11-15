2d23489d7a898a55a08e22ef95715c2bda394ee2
Jonathan Hess <hessjc@google.com>
2022-11-15 14:57:40 -0700

    feat: add release PR job and release job  GH-66

---
+++ tools/publish-installer.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -euxo
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR=$( dirname "$SCRIPT_DIR")
+
+cd "$PROJECT_DIR"
+
+##
+# Release Process
+if [[ -n ${RELEASE_TEST_BUILD_ID:-} ]] ; then
+  VERSION="${RELEASE_TEST_BUILD_ID}"
+else
+  VERSION="v$(cat "$PROJECT_DIR/version.txt" | tr -d "\n")"
+fi
+
+# Upload the installer files to the storage bucket
+gcloud storage --project cloud-sql-connectors cp installer/install.sh "gs://cloud-sql-connectors/cloud-sql-proxy-operator-dev/${VERSION}/install.sh"
+gcloud storage --project cloud-sql-connectors cp installer/cloud-sql-proxy-operator.yaml "gs://cloud-sql-connectors/cloud-sql-proxy-operator-dev/${VERSION}/cloud-sql-proxy-operator.yaml"
+
