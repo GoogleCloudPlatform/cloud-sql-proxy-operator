a8ea09c6a9ca466c80274241b1aeb31998a24b47
Jonathan Hess <hessjc@google.com>
2022-11-18 09:50:37 -0700

    feat: Finish pod webhook and workload reconcile implementation

--- internal/controller/authproxyworkload_controller_test.go
+++ internal/controller/authproxyworkload_controller_test.go
@@ -21,7 +21,6 @@ import (
 	"testing"
 
 	"go.uber.org/zap/zapcore"
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
--- internal/controller/pod_controller.go
+++ internal/controller/pod_controller.go
@@ -20,11 +20,14 @@ import (
 	"net/http"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/json"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
+	cloudsqlapi "github.com/GoogleCloudPlatform/cloud-sql-proxy-operator/internal/api/v1alpha1"
 	"github.com/GoogleCloudPlatform/cloud-sql-proxy-operator/internal/workload"
 )
 
@@ -56,7 +59,52 @@ func (a *PodAdmissionWebhook) Handle(ctx context.Context, req admission.Request)
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 
-	//TODO implement pod webhook
+	var (
+		updated           bool
+		instList          = &cloudsqlapi.AuthProxyWorkloadList{}
+		matchingInstances []*cloudsqlapi.AuthProxyWorkload
+		wlConfigErr       error
+	)
+
+	// List all the AuthProxyWorkloads in the same namespace.
+	// To avoid privilege escalation, the operator requires that the AuthProxyWorkload
+	// may only affect pods in the same namespace.
+	err = a.Client.List(ctx, instList, client.InNamespace(wl.Object().GetNamespace()))
+	if err != nil {
+		l.Error(err, "Unable to list CloudSqlClient resources in webhook",
+			"kind", req.Kind.Kind, "ns", req.Namespace, "name", req.Name)
+		return admission.Errored(http.StatusInternalServerError,
+			fmt.Errorf("unable to list CloudSqlClient resources"))
+	}
+
+	// List the owners of this pod.
+	owners, err := a.listOwners(ctx, wl.Object())
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError,
+			fmt.Errorf("there is an AuthProxyWorkloadConfiguration error reconciling this workload %v", err))
+	}
+
+	// Reconfigure the PodSpec
+	updated, matchingInstances, wlConfigErr = a.updater.ConfigurePodProxies(instList, wl, owners)
+
+	if wlConfigErr != nil {
+		l.Error(wlConfigErr, "Unable to reconcile workload result in webhook: "+wlConfigErr.Error(),
+			"kind", req.Kind.Kind, "ns", req.Namespace, "name", req.Name)
+		return admission.Errored(http.StatusInternalServerError,
+			fmt.Errorf("there is an AuthProxyWorkloadConfiguration error reconciling this workload %v", wlConfigErr))
+	}
+
+	// If the pod was updated, log some information
+	if updated {
+		l.Info(fmt.Sprintf("Workload operation %s on kind %s named %s/%s required an update",
+			req.Operation, req.Kind, req.Namespace, req.Name))
+		for _, inst := range matchingInstances {
+			l.Info(fmt.Sprintf("inst %v %v/%v updated at instance resource version %v",
+				wl.Object().GetObjectKind().GroupVersionKind().String(),
+				wl.Object().GetNamespace(), wl.Object().GetName(),
+				inst.GetResourceVersion()))
+		}
+	}
 
 	// Marshal the updated Pod and prepare to send a response
 	result := wl.Object()
@@ -69,4 +117,48 @@ func (a *PodAdmissionWebhook) Handle(ctx context.Context, req admission.Request)
 	}
 
 	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledRes)
+}
+
+// listOwners returns the list of this object's owners and its extended owners
+func (a *PodAdmissionWebhook) listOwners(ctx context.Context, object client.Object) ([]workload.Workload, error) {
+	l := logf.FromContext(ctx)
+	var owners []workload.Workload
+
+	for _, r := range object.GetOwnerReferences() {
+		key := client.ObjectKey{Namespace: object.GetNamespace(), Name: r.Name}
+		var owner client.Object
+
+		wl, err := workload.WorkloadForKind(r.Kind)
+		if err != nil {
+			owner = &metav1.PartialObjectMetadata{
+				TypeMeta: metav1.TypeMeta{Kind: r.Kind, APIVersion: r.APIVersion},
+			}
+		} else {
+			owners = append(owners, wl)
+			owner = wl.Object()
+		}
+
+		err = a.Client.Get(ctx, key, owner)
+		if err != nil {
+			switch t := err.(type) {
+			case *apierrors.StatusError:
+				// Ignore when the owner is not found. Sometimes owners no longer exist.
+				if t.ErrStatus.Reason == metav1.StatusReasonNotFound {
+					continue
+				}
+			}
+
+			l.Info("could not get owner ", "owner", r.String(), "err", err)
+			return owners, err
+		}
+
+		// recursively call for the owners of the owner, and append those.
+		// So that we reach Pod --> ReplicaSet --> Deployment
+		ownerOwners, err := a.listOwners(ctx, owner)
+		owners = append(owners, ownerOwners...)
+		if err != nil {
+			return owners, err
+		}
+	}
+	return owners, nil
 }
