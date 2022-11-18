8803bec5107ff3a18969f6f6d0814af2988c51dd
Jonathan Hess <hessjc@google.com>
2022-11-18 10:32:01 -0700

    test: move test case from helpers to integration package
    TestModifiesNewDeployment and TestModifiesExistingDeployment test case implementation
    was shared between e2e and integration tests. That will no longer work.

--- internal/testintegration/integration_test.go
+++ internal/testintegration/integration_test.go
@@ -18,11 +18,20 @@
 package testintegration_test
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/GoogleCloudPlatform/cloud-sql-proxy-operator/internal/testhelpers"
 	"github.com/GoogleCloudPlatform/cloud-sql-proxy-operator/internal/testintegration"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 func TestMain(m *testing.M) {
@@ -65,12 +74,190 @@ func TestDeleteResource(t *testing.T) {
 }
 
 func TestModifiesNewDeployment(t *testing.T) {
-	tctx := testCaseParams(t, "modifynew")
-	testhelpers.TestModifiesNewDeployment(tctx)
+	tp := testCaseParams(t, "modifynew")
+
+	testhelpers.CreateOrPatchNamespace(tp.Ctx, tp)
+
+	const (
+		pwlName            = "newdeploy"
+		deploymentAppLabel = "busybox"
+	)
+	ctx := tp.Ctx
+	key := types.NamespacedName{Name: pwlName, Namespace: tp.Namespace}
+
+	t.Log("Creating AuthProxyWorkload")
+	err := testhelpers.CreateAuthProxyWorkload(ctx, tp, key, deploymentAppLabel, tp.ConnectionString, "Deployment")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log("Waiting for AuthProxyWorkload operator to begin the reconcile loop")
+	_, err = testhelpers.GetAuthProxyWorkloadAfterReconcile(ctx, tp, key)
+	if err != nil {
+		t.Error("unable to create AuthProxyWorkload", err)
+		return
+	}
+
+	t.Log("Creating deployment")
+	d := &appsv1.Deployment{}
+	err = testhelpers.CreateWorkload(ctx, tp, key, deploymentAppLabel, testhelpers.BusyboxDeployYaml, d)
+
+	if err != nil {
+		t.Error("unable to create deployment", err)
+		return
+	}
+	time.Sleep(time.Second * 5)
+	t.Log("Creating deployment replicas")
+	_, _, err = createDeploymentReplicaSetAndPods(tp, d)
+	if err != nil {
+		t.Error("unable to create pods", err)
+		return
+	}
+	time.Sleep(time.Second * 5)
+
+	testhelpers.RetryUntilSuccess(t, 5, 5, func() error {
+		dep := &appsv1.Deployment{}
+		err := tp.Client.Get(tp.Ctx, key, dep)
+		if err != nil {
+			return err
+		}
+		annKey := fmt.Sprintf("cloudsql.cloud.google.com/req-%s-%s", tp.Namespace, pwlName)
+		wantAnn := "1"
+		var gotAnn string
+		if dep.Spec.Template.Annotations != nil {
+			gotAnn = dep.Spec.Template.Annotations[annKey]
+		}
+		if len(dep.Spec.Template.Annotations) == 0 {
+			return fmt.Errorf("Expected annotations")
+		}
+		if wantAnn != gotAnn {
+			return fmt.Errorf("got %s, want %s for annotation named %s", gotAnn, wantAnn, annKey)
+		}
+		return nil
+	})
+
+	testhelpers.ExpectContainerCount(tp, d.GetNamespace(), d.Spec.Selector, 2, "all")
+
 }
 
 func TestModifiesExistingDeployment(t *testing.T) {
-	tctx := testCaseParams(t, "modifyexisting")
-	testRemove := testhelpers.TestModifiesExistingDeployment(tctx)
-	testRemove()
+	const (
+		pwlName            = "db-mod"
+		deploymentName     = "deploy-mod"
+		deploymentAppLabel = "existing-mod"
+	)
+	var (
+		tp  = testCaseParams(t, "modifyexisting")
+		ctx = tp.Ctx
+	)
+
+	testhelpers.CreateOrPatchNamespace(ctx, tp)
+	t.Logf("Creating namespace %v", tp.Namespace)
+
+	pKey := types.NamespacedName{Name: pwlName, Namespace: tp.Namespace}
+	dKey := types.NamespacedName{Name: deploymentName, Namespace: tp.Namespace}
+
+	t.Log("Creating deployment")
+	d := &appsv1.Deployment{}
+	err := testhelpers.CreateWorkload(ctx, tp, dKey, deploymentAppLabel, testhelpers.BusyboxDeployYaml, d)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, _, err = createDeploymentReplicaSetAndPods(tp, d)
+	if err != nil {
+		t.Errorf("Unable to create pods and replicaset for deployment, %v", err)
+		return
+	}
+
+	// expect 1 container... no cloudsql instance yet
+	testhelpers.ExpectContainerCount(tp, d.GetNamespace(), d.Spec.Selector, 1, "all")
+
+	t.Log("Creating cloud sql instance")
+	err = testhelpers.CreateAuthProxyWorkload(ctx, tp, pKey, deploymentAppLabel, tp.ConnectionString, "Deployment")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log("Waiting for cloud sql instance to begin the reconcile loop ")
+	_, err = testhelpers.GetAuthProxyWorkloadAfterReconcile(ctx, tp, pKey)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	//TODO implement the new reconcile algorithm before finishing this test.
+	// Then, we should assert 2 containers on all pods.
+
+}
+
+// createDeploymentReplicaSetAndPods mimics the behavior of the deployment controller
+// built into kubernetes. It creates one ReplicaSet and DeploymentSpec.Replicas pods
+// with the correct labels and ownership annotations as if it were in a live cluster.
+// This will make it easier to test and debug the behavior of our pod injection webhooks.
+func createDeploymentReplicaSetAndPods(tp *testhelpers.TestCaseParams, d *appsv1.Deployment) (*appsv1.ReplicaSet, []*corev1.Pod, error) {
+	podTemplateHash := strconv.FormatUint(rand.Uint64(), 16)
+	rs := &appsv1.ReplicaSet{
+		TypeMeta: metav1.TypeMeta{Kind: "ReplicaSet", APIVersion: "apps/metav1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-%s", d.Name, podTemplateHash),
+			Namespace: d.Namespace,
+			Annotations: map[string]string{
+				"deployment.kubernetes.io/desired-replicas": "2",
+				"deployment.kubernetes.io/max-replicas":     "3",
+				"deployment.kubernetes.io/revision":         "1",
+			},
+			Generation: 1,
+			Labels: map[string]string{
+				"app":               d.Spec.Template.Labels["app"],
+				"enablewait":        "yes",
+				"pod-template-hash": podTemplateHash,
+			},
+		},
+		Spec: appsv1.ReplicaSetSpec{
+			Replicas: d.Spec.Replicas,
+			Selector: d.Spec.Selector,
+			Template: d.Spec.Template,
+		},
+	}
+
+	controllerutil.SetOwnerReference(d, rs, tp.Client.Scheme())
+	err := tp.Client.Create(tp.Ctx, rs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var replicas int32
+	if d.Spec.Replicas != nil {
+		replicas = *d.Spec.Replicas
+	} else {
+		replicas = 1
+	}
+	var pods []*corev1.Pod
+	for i := int32(0); i < replicas; i++ {
+		podID := strconv.FormatUint(uint64(rand.Uint32()), 16)
+		p := &corev1.Pod{
+			TypeMeta: metav1.TypeMeta{Kind: "Pod", APIVersion: "v1"},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:       fmt.Sprintf("%s-%s-%s", d.Name, podTemplateHash, podID),
+				Namespace:  d.Namespace,
+				Generation: 1,
+				Labels: map[string]string{
+					"app":               d.Spec.Template.Labels["app"],
+					"enablewait":        "yes",
+					"pod-template-hash": podTemplateHash,
+				},
+			},
+			Spec: d.Spec.Template.Spec,
+		}
+		controllerutil.SetOwnerReference(rs, p, tp.Client.Scheme())
+		err = tp.Client.Create(tp.Ctx, p)
+		if err != nil {
+			return rs, nil, err
+		}
+		pods = append(pods, p)
+	}
+	return rs, pods, nil
 }
