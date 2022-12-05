736fcb0f29b0ea18e716daee479265e1d517d9f7
Jonathan Hess <hessjc@google.com>
2022-12-05 16:29:52 -0700

    fix: repair testcases for putting containers on pods.

--- internal/controller/authproxyworkload_controller_test.go
+++ internal/controller/authproxyworkload_controller_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"go.uber.org/zap/zapcore"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
