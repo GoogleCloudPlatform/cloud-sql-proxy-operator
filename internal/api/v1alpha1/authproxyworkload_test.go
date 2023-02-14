0caaa02f2755fca418f574643704b6d7a8260032
Jonathan Hess <hessjc@google.com>
2023-02-14 11:05:26 -0700

    wip: validating webhook not working.

---
+++ internal/api/v1alpha1/authproxyworkload_test.go
@@ -0,0 +1,121 @@
+package v1alpha1_test
+
+import (
+	"testing"
+
+	cloudsqlapi "github.com/GoogleCloudPlatform/cloud-sql-proxy-operator/internal/api/v1alpha1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestAuthProxyWorkload_ValidateCreateUpdate(t *testing.T) {
+	type testcase struct {
+		desc            string
+		spec            cloudsqlapi.AuthProxyWorkloadSpec
+		oldSpec         *cloudsqlapi.AuthProxyWorkloadSpec
+		wantCreateValid bool
+		wantUpdateValid bool
+	}
+
+	data := []*testcase{
+		{
+			desc: "happy path",
+			spec: cloudsqlapi.AuthProxyWorkloadSpec{
+				Workload: cloudsqlapi.WorkloadSelectorSpec{Kind: "Deployment", Name: "webapp"},
+			},
+			wantCreateValid: true,
+			wantUpdateValid: true,
+		},
+		{
+			desc: "happy path labels",
+			spec: cloudsqlapi.AuthProxyWorkloadSpec{
+				Workload: cloudsqlapi.WorkloadSelectorSpec{Kind: "Deployment", Selector: &v1.LabelSelector{MatchLabels: map[string]string{"app": "sample"}}},
+			},
+			wantCreateValid: true,
+			wantUpdateValid: true,
+		},
+		{
+			desc: "invalid labels and name both set",
+			spec: cloudsqlapi.AuthProxyWorkloadSpec{
+				Workload: cloudsqlapi.WorkloadSelectorSpec{Kind: "Deployment", Name: "webapp", Selector: &v1.LabelSelector{MatchLabels: map[string]string{"app": "sample"}}},
+			},
+			wantCreateValid: false,
+			wantUpdateValid: true,
+		},
+		{
+			desc: "invalid labels and name not set",
+			spec: cloudsqlapi.AuthProxyWorkloadSpec{
+				Workload: cloudsqlapi.WorkloadSelectorSpec{Kind: "Deployment"},
+			},
+			wantCreateValid: false,
+			wantUpdateValid: true,
+		},
+		{
+			desc: "happy path update",
+			spec: cloudsqlapi.AuthProxyWorkloadSpec{
+				Workload: cloudsqlapi.WorkloadSelectorSpec{Kind: "Deployment", Name: "webapp"},
+				Instances: []cloudsqlapi.InstanceSpec{{
+					ConnectionString: "proj:region:db2",
+					PortEnvName:      "DB_PORT",
+				}},
+			},
+			oldSpec: &cloudsqlapi.AuthProxyWorkloadSpec{
+				Workload: cloudsqlapi.WorkloadSelectorSpec{Kind: "Deployment", Name: "webapp"},
+				Instances: []cloudsqlapi.InstanceSpec{{
+					ConnectionString: "proj:region:db1",
+					PortEnvName:      "DB_PORT",
+				}},
+			},
+			wantCreateValid: true,
+			wantUpdateValid: true,
+		},
+	}
+
+	for _, tc := range data {
+		t.Run(tc.desc, func(t *testing.T) {
+			p := cloudsqlapi.AuthProxyWorkload{
+				ObjectMeta: v1.ObjectMeta{Name: "sample"},
+				Spec:       tc.spec,
+			}
+			err := p.ValidateCreate()
+			switch {
+			case tc.wantCreateValid && err != nil:
+				t.Errorf("wants create valid, got error %v", err)
+				printFieldErrors(t, err)
+			case !tc.wantCreateValid && err == nil:
+				t.Errorf("wants an error on create, got no error")
+			default:
+				t.Logf("create passed %s", tc.desc)
+				// test passes, do nothing.
+			}
+
+			if tc.oldSpec == nil {
+				return
+			}
+
+			oldP := cloudsqlapi.AuthProxyWorkload{Spec: *tc.oldSpec}
+
+			err = p.ValidateUpdate(&oldP)
+			switch {
+			case tc.wantUpdateValid && err != nil:
+				t.Errorf("wants create valid, got error %v", err)
+			case !tc.wantUpdateValid && err == nil:
+				t.Errorf("wants an error on create, got no error")
+			default:
+				t.Logf("update passed %s", tc.desc)
+				// test passes, do nothing.
+			}
+		})
+	}
+}
+
+func printFieldErrors(t *testing.T, err error) {
+	t.Helper()
+	statusErr, ok := err.(*apierrors.StatusError)
+	if ok {
+		t.Errorf("Field status errors: ")
+		for _, v := range statusErr.Status().Details.Causes {
+			t.Errorf("   %v %v: %v ", v.Field, v.Type, v.Message)
+		}
+	}
+}
--- internal/api/v1alpha1/authproxyworkload_webhook.go
+++ internal/api/v1alpha1/authproxyworkload_webhook.go
@@ -15,7 +15,13 @@
 package v1alpha1
 
 import (
+	"fmt"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/validation"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -30,10 +36,7 @@ func (r *AuthProxyWorkload) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-//+kubebuilder:webhook:path=/mutate-cloudsql-cloud-google-com-v1alpha1-authproxyworkload,mutating=true,failurePolicy=fail,sideEffects=None,groups=cloudsql.cloud.google.com,resources=authproxyworkloads,verbs=create;update,versions=v1alpha1,name=mauthproxyworkload.kb.io,admissionReviewVersions=v1
-
+// +kubebuilder:webhook:path=/mutate-cloudsql-cloud-google-com-v1alpha1-authproxyworkload,mutating=true,failurePolicy=fail,sideEffects=None,groups=cloudsql.cloud.google.com,resources=authproxyworkloads,verbs=create;update,versions=v1alpha1,name=mauthproxyworkload.kb.io,admissionReviewVersions=v1
 var _ webhook.Defaulter = &AuthProxyWorkload{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
@@ -45,31 +48,88 @@ func (r *AuthProxyWorkload) Default() {
 	}
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:path=/validate-cloudsql-cloud-google-com-v1alpha1-authproxyworkload,mutating=false,failurePolicy=fail,sideEffects=None,groups=cloudsql.cloud.google.com,resources=authproxyworkloads,verbs=create;update,versions=v1alpha1,name=vauthproxyworkload.kb.io,admissionReviewVersions=v1
-
+// +kubebuilder:webhook:path=/validate-cloudsql-cloud-google-com-v1alpha1-authproxyworkload,mutating=false,failurePolicy=fail,sideEffects=None,groups=cloudsql.cloud.google.com,resources=authproxyworkloads,verbs=create;update,versions=v1alpha1,name=vauthproxyworkload.kb.io,admissionReviewVersions=v1
 var _ webhook.Validator = &AuthProxyWorkload{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *AuthProxyWorkload) ValidateCreate() error {
 	authproxyworkloadlog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return r.Validate()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *AuthProxyWorkload) ValidateUpdate(_ runtime.Object) error {
 	authproxyworkloadlog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return r.Validate()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *AuthProxyWorkload) ValidateDelete() error {
 	authproxyworkloadlog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
+}
+
+func (r *AuthProxyWorkload) Validate() error {
+	var allErrs field.ErrorList
+
+	allErrs = append(allErrs, validation.ValidateLabelName(r.Name, field.NewPath("metadata", "name"))...)
+	allErrs = append(allErrs, validateSpec(&r.Spec, field.NewPath("spec"))...)
+
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(
+		schema.GroupKind{
+			Group: GroupVersion.Group,
+			Kind:  "AuthProxyWorkload"},
+		r.Name, allErrs)
+}
+
+func validateSpec(spec *AuthProxyWorkloadSpec, f *field.Path) field.ErrorList {
+	var allErrs field.ErrorList
+
+	allErrs = append(allErrs, validateWorkload(&spec.Workload, f.Child("workload"))...)
+
+	// TODO: Validate the other fields in spec
+	// allErrs = append(allErrs, validateAuthProxyContainer(spec.AuthProxyContainer, f.Child("authProxyContainer"))...)
+	// allErrs = append(allErrs, validateAuthentication(spec.Authentication, f.Child("authentication"))...)
+	// allErrs = append(allErrs, validateAuthentication(spec.Instances, f.Child("instances"))...)
+
+	return allErrs
+}
+
+var supportedKinds = []string{"CronJob", "Job", "StatefulSet", "Deployment", "DaemonSet", "ReplicaSet", "Pod"}
+
+func validateWorkload(spec *WorkloadSelectorSpec, f *field.Path) field.ErrorList {
+	var errs field.ErrorList
+	if spec.Selector != nil {
+		verr := validation.ValidateLabelSelector(spec.Selector, validation.LabelSelectorValidationOptions{}, f.Child("selector"))
+		errs = append(errs, verr...)
+	}
+
+	if spec.Name != "" && spec.Selector != nil {
+		errs = append(errs, field.Invalid(f.Child("name"), spec,
+			"WorkloadSelectorSpec must specify either name or selector. Both were set."))
+	}
+	if spec.Name == "" && spec.Selector == nil {
+		errs = append(errs, field.Invalid(f.Child("name"), spec,
+			"WorkloadSelectorSpec must specify either name or selector. Neither was set."))
+	}
+
+	_, gk := schema.ParseKindArg(spec.Kind)
+	var found bool
+	for _, kind := range supportedKinds {
+		if kind == gk.Kind {
+			found = true
+			break
+		}
+	}
+	if !found {
+		errs = append(errs, field.Invalid(f.Child("kind"), spec.Kind,
+			fmt.Sprintf("Kind was %q, must be one of CronJob, Job, StatefulSet, Deployment, DaemonSet or Pod", gk.Kind)))
+
+	}
+
+	return errs
 }
