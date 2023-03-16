bbd849940247c339b191fe2373f6d9f18cf63d13
Jonathan Hess <hessjc@google.com>
2023-03-16 14:10:56 -0600

    fix: code formatting cleanup

--- infra/permissions/main.tf
+++ infra/permissions/main.tf
@@ -17,8 +17,8 @@
 terraform {
   required_providers {
     google = {
-      source                = "hashicorp/google"
-      version               = "4.56.0"
+      source  = "hashicorp/google"
+      version = "4.56.0"
     }
   }
 }
--- infra/resources/main.tf
+++ infra/resources/main.tf
@@ -17,12 +17,12 @@
 terraform {
   required_providers {
     google = {
-      source                = "hashicorp/google"
-      version               = "4.56.0"
+      source  = "hashicorp/google"
+      version = "4.56.0"
     }
     google-beta = {
-      source                = "hashicorp/google-beta"
-      version               = "4.56.0"
+      source  = "hashicorp/google-beta"
+      version = "4.56.0"
     }
   }
 }
